57ac45d8751a4ca10fce1218951291b6d6dd1031
Hugh Noble <hugh.noble@gmail.com>
2022-12-14 20:58:20 +0000

    Day 14 part 2

--- app/Console/Commands/Day14.php
+++ app/Console/Commands/Day14.php
@@ -12,6 +12,7 @@ class Day14 extends Command
     private static string $SAND = "s";
     private static string $ROCK = "#";
     private static string $AIR = ".";
+    private static string $SAND_HOLE = "+";
 
     private static int $SAND_ORIGIN = 500;
 
@@ -59,7 +60,7 @@ class Day14 extends Command
         $i = 0;
         while (true) {
             try {
-                $grid = $this->drop($grid, self::$SAND_ORIGIN, 0);
+                $grid = $this->drop($grid, self::$SAND_ORIGIN, 0, false);
             } catch (Exception $e) {
                 break;
             }
@@ -68,36 +69,71 @@ class Day14 extends Command
         }
 
         $this->info("Part 1: " . $i);
+
+        $rockCoords = collect(explode("\n", $contents))
+            ->map(fn($item) => $this->parseToCoords($item));
+        
+        $grid = $this->addRocks($this->buildGrid($rockCoords), $rockCoords);
+        $grid = $this->addFloor($grid);
+
+        $i = 0;
+        while (true) {
+            try {
+                $grid = $this->drop($grid, self::$SAND_ORIGIN, 0, true);
+            } catch (Exception $e) {
+                break;
+            }
+
+            $i++;
+        }
+
+        $this->info("Part 2: " . $i);
         
         return Command::SUCCESS;
     }
 
-    private function drop(Collection $grid, int $x, int $y): Collection
+    private function drop(Collection $grid, int $x, int $y, bool $canExtend): Collection
     {
         $this->debug("Going to try dropping %d:%d", $x, $y);
-        if (!$grid->has($y) || !$grid->get($y)->has($x)) {
+        if (!$grid->has($y)) {
             throw new Exception("Out of range");
+        }
+
+        if (!$grid->get($y)->has($x)) {
+            if (!$canExtend) {
+                throw new Exception("Out of range");
+            }
+
+            $this->debug("Extending grid with x: " . $x);
+            $grid = $this->extendGrid($grid, $x);
+
+            $x = self::$SAND_ORIGIN;
+            $y = 0;
         }
 
         $position = $grid->get($y)->get($x);
 
-        if ($position !== self::$AIR) {
+        if (!in_array($position, [self::$AIR, self::$SAND_HOLE])) {
             throw new Exception("Position not free");
         }
 
         foreach ($grid->skip($y) as $key => $line) {
             $position = $line->get($x);
 
-            if ($position !== self::$AIR) {
+            if (!in_array($position, [self::$AIR, self::$SAND_HOLE])) {
                 $key--;
                 break;
             }
         }
 
+        if ($position === self::$SAND_HOLE) {
+            throw new Exception("Out of range");
+        }
+
         foreach ([-1, 1] as $xModifier) {
             try {
                 $this->debug("Trying %s:%s", $x + $xModifier, $key + 1);
-                $grid = $this->drop($grid, $x + $xModifier, $key + 1);
+                $grid = $this->drop($grid, $x + $xModifier, $key + 1, $canExtend);
                 $this->debug("Dropped at %s:%s", $x + $xModifier, $key + 1);
                 return $grid;
             } catch (Exception $e) {
@@ -135,6 +171,13 @@ class Day14 extends Command
         return $grid;
     }
 
+    private function addFloor(Collection $grid): Collection
+    {
+        return $grid
+            ->add($grid->first()->map(fn($item) => self::$AIR))
+            ->add($grid->first()->map(fn($item) => self::$ROCK));
+    }
+
     private function buildGrid(Collection $rockCoords): Collection
     {
         $flattened = $rockCoords->flatten(1);
@@ -150,8 +193,18 @@ class Day14 extends Command
             ->flip()
             ->map(fn($item) => self::$AIR);
         
-        return collect(range($minY, $maxY))
+        $grid = collect(range($minY, $maxY))
             ->map(fn($item) => collect($emptyRow));
+        
+        $grid->get(0)->put(self::$SAND_ORIGIN, self::$SAND_HOLE);
+
+        return $grid;
+    }
+
+    private function extendGrid(Collection $grid, int $x) {
+        $grid = $grid->map(fn($item) => $item->put($x, self::$AIR));
+        $grid->put($grid->keys()->last(), $grid->last()->map(fn($item) => self::$ROCK));
+        return $grid->map(fn($item) => $item->sortKeys());
     }
 
     private function renderGrid(Collection $grid): void
